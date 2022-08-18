af3865ad81e8f23d4a4be65377ef58f7c441a1b7
Andrey Dmitriev <admit8490@gmail.com>
2022-08-18 17:09:52 +0200

    task and boilerpalte fo merge sort

---
+++ Merge sort.js
@@ -0,0 +1,11 @@
+/* TASK
+we can recursively divide the original input in two until we reach the base case of an array with one item.
+A single-item array is naturally sorted, so then we can start combining
+A good way to implement this is to write one function, for instance merge, which is responsible for merging two sorted arrays, and another function, for instance mergeSort, which is responsible for the recursion that produces single-item arrays to feed into merge.
+*/
+
+function mergeSort(array) {
+  // Only change code below this line
+  return array;
+  // Only change code above this line
+}
