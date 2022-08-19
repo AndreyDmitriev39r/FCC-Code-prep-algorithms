8b85139f93a86ce607f7a269cbdb6d7971072e4f
Andrey Dmitriev <admit8490@gmail.com>
2022-08-19 14:35:01 +0200

    solved: binary search

--- Binary search.js
+++ Binary search.js
@@ -3,17 +3,38 @@ Write a function binarySearch that implements the binary search algorithm on an 
 */
 
 function binarySearch(searchList, value) {
+  //variables
   let arrayPath = [];
-  return arrayPath;
+  let len = searchList.length;
+  let newSearchList = searchList.slice();
+  let lastFound;
+  let lastFoundIndex;
+  //searching  
+  while (len > 0) {
+    lastFoundIndex = Math.floor((len - 1) / 2);
+    lastFound = newSearchList[lastFoundIndex];
+    arrayPath.push(lastFound);    
+    if (value === lastFound) {
+      return arrayPath;
+    }
+    else if (value > lastFound) {
+      newSearchList = newSearchList.slice(lastFoundIndex + 1);
+    }
+    else if (value < lastFound) {
+      newSearchList = newSearchList.slice(0, lastFoundIndex);
+    }
+    len = newSearchList.length;    
+  }
+  return 'Value Not Found';
 }
 
 
-//TEST
+// //TEST
 const testArray = [
   0, 1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
   23, 49, 70
 ];
-
+// console.log(binarySearch(testArray, 1));
 const testValuesAndExpected = [
   [0, [13, 5, 2, 0]],
   [1, [13, 5, 2, 0, 1]],
