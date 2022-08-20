b396934a64b852c79783f96a9ba98ec4df2f2aac
Andrey Dmitriev <admit8490@gmail.com>
2022-08-21 01:58:07 +0200

    quick sort: boilerplate

---
+++ quick sort.js
@@ -0,0 +1,10 @@
+/* TASK
+In this method, a pivot value is chosen in the original array.
+The array is then partitioned into two subarrays of values less than and greater than the pivot value. We then combine the result of recursively calling the quick sort algorithm on both sub-arrays. This continues until the base case of an empty or single-item array is reached, which we return.
+*/
+
+function quickSort(array) {
+  // Only change code below this line
+  return array;
+  // Only change code above this line
+}
