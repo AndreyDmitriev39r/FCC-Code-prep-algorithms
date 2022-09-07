014a05b38b59275c6a04a41c60480a49755ca858
Andrey Dmitriev <admit8490@gmail.com>
2022-09-07 15:16:29 +0200

    quicksort tests added

--- quick sort.js
+++ quick sort.js
@@ -8,3 +8,19 @@ function quickSort(array) {
   return array;
   // Only change code above this line
 }
+
+// TESTS
+
+const testCases = [
+  [1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92],
+  [4, 6, 77, 11, 1, 1, 5, 3, 66, 200, 122],
+  [586, 15, 3, 76, 25, 12, 1, 12, 2, 2]
+]
+
+for (let test of testCases) {
+  let copy = test.slice();
+  console.log('CASE ', test);
+  console.log('EXPECTED ', copy.sort((a, b) => a - b));
+  console.log('FACT ', quickSort(test));
+  console.log('-----------------------------')
+}
